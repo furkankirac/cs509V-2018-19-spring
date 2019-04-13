a824bf8e7172fb998a0f89e0c196d5d2a1173ead
Furkan Kıraç <fkirac@gmail.com>
2019-04-13 15:12:08 +0300

    Pixel is defined. Image class implemented from Matrix<Pixel>.

--- app6.cpp
+++ app6.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+using Brightness = unsigned char;
+
+struct Pixel // Colored Pixel
+{
+    Brightness red, green, blue;
+};
+
 struct MatrixBase
 {
     // abstract class because of = 0;
@@ -43,7 +50,39 @@ struct Matrix : public MatrixBase
             {
                 if(j!=0)
                     cout << " ";
-                cout << (*this)(i, j);
+
+                if constexpr(!is_same<T, Pixel>::value)
+                    cout << (*this)(i, j);
+            }
+            cout << endl;
+        }
+    }
+};
+
+struct Image : public Matrix<Pixel>
+{
+    // 1st method for c-tor
+//    Image(int nRows, int nCols) :
+//        Matrix<Pixel>{nRows, nCols}
+//    { }
+
+    // 2nd method: this inherits c-tors of base class Matrix<Pixel>
+    // because normally c-tors are not inherited by default
+    // you need to explicitly say it
+    using Matrix<Pixel>::Matrix;
+
+    void print() const override
+    {
+        for(int i=0; i<nRows; ++i)
+        {
+            for(int j=0; j<nCols; ++j)
+            {
+                if(j!=0)
+                    cout << " ";
+                const auto& pix = (*this)(i, j);
+                cout << "(" << (int)pix.red << ","
+                     << (int)pix.blue << ","
+                     << (int)pix.green << ")";
             }
             cout << endl;
         }
@@ -73,6 +112,12 @@ int main(int argc, char* argv[])
         cout << "---------------" << endl;
         m->print();
     }
+
+    cout << "Printing image now." << endl;
+    auto img = Image{10, 10};
+    img.print();
+
+
 
 //    for(auto m : V) // m is a MatrixBase*
 //    {
