b03ff913c662783323b419584e9687cbc8430c31
Furkan Kıraç <fkirac@gmail.com>
2019-05-04 15:05:17 +0300

    compile-time variadic parameters. parameter packs. pack expansions. fold expressions.

--- app13.cpp
+++ app13.cpp
@@ -25,47 +25,14 @@
 // RVO: return value optimization
 // CRTP: curiously recurring template pattern
 
-
 #include <iostream>
-#include <stdio.h>
-
 
 using namespace std;
 
 template<typename T>
 struct TypeDisplayer;
 
-namespace my
-{
-    template<typename T1, typename T2>  struct is_same       { static const bool value = false; };
-    template<typename T>                struct is_same<T, T> { static const bool value = true; };
-//    template<typename T>                struct is_same<T, char> {
-//        static const bool value = true;
-//        static const bool is_char = true;
-//    };
-
-//    struct is_same_int_float    { static const bool value = false; };
-//    struct is_same_int_int      { static const bool value = true; };
-
-    template<typename T>    struct remove_const             { using type = T; };
-    template<typename K>    struct remove_const<const K>    { using type = K; };
-    template<typename K>    struct remove_const<const K*>   { using type = K*; };
-
-    template<typename T>    struct remove_volatile                  { using type = T; };
-    template<typename K>    struct remove_volatile<volatile K>      { using type = K; };
-    template<typename K>    struct remove_volatile<volatile K*>     { using type = K*; };
-
-    template<typename T>    struct remove_cv
-    {
-        using type = typename remove_const< typename remove_volatile<T>::type >::type;
-    };
-
-    template<typename T>    struct remove_reference      { using type = T; };
-    template<typename K>    struct remove_reference<K&>  { using type = K; };
-    template<typename K>    struct remove_reference<K&&> { using type = K; };
-}
-
-int sumAllIntegers(const char* title, ...)
+int sumAllIntegers(const char* title...) // run-time resolved variadic parameters example
 {
     va_list args;
     va_start(args, title);
@@ -84,26 +51,38 @@ int sumAllIntegers(const char* title, ...)
     return sum;
 }
 
+template<int first, int ... rest>
+inline void printInts()
+{
+    cout << first << endl;
+    if constexpr(sizeof...(rest) > 0)
+        printInts<rest...>();
+}
 
+template<typename First, typename ... Rest>
+inline void printAll(First first, Rest... rest)
+{
+    cout << first << endl;
+    if constexpr(sizeof...(Rest) > 0)
+        printAll<Rest...>(rest...);
+}
+
+
+template<typename ... All>
+void print(All ... all)
+{
+    ((cout << all << endl), ...); // fold expression
+}
 
 
 int main(int argc, char* argv[])
 {
-//    auto b1 = my::is_same<int, float>::value;
-//    auto b2 = my::is_same<double, double>::value;
+//    cout << sumAllIntegers("HELLO", 10, 20, 30, 5, 100, 253, 5) << endl;
+    printInts<10, 20, 30>();
 
-////    auto a = TypeDisplayer< my::remove_cv<const volatile int>::type >{};
-////    auto b = TypeDisplayer< my::remove_cv<volatile char*>::type >{};
-
-//    auto r = TypeDisplayer< remove_reference<int&>::type >{};
-
-////    auto c = my::is_same<double, char>::is_char;
-
-//    cout << b1 << endl;
-//    cout << b2 << endl;
-
-    cout << sumAllIntegers("HELLO", 10, 20, 30, 5, 100, 253, 5) << endl;
-
+//    printAll<int, int, double, float, char>(10, 20, 30.5, 20.3f, 'c');
+//    printAll(10, 20, 30.5, 20.3f, 'c');
+    print(10, 20, 30.5, 20.3f, 'c');
 
     return 0;
 }
