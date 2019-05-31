9aebb528a68e325385fe1d916f9a9771c7a915b5
Furkan Kıraç <fkirac@gmail.com>
2019-05-31 14:08:18 +0300

    Example #3.

--- app18.cpp
+++ app18.cpp
@@ -44,22 +44,70 @@ int main(int argc, char* argv[])
             cout << str << endl;
     };
 
-    printIt(filter(s, [](const string& str) {
-        return str.at(0) == 'h';
-    }));
+    struct Lambda1
+    {
+        auto operator() (const string& str) const {
+            return str.at(0) == 'h';
+        }
+    };
+
+    printIt(filter(s, Lambda1{}));
+//    printIt(filter(s, [](const string& str) {
+//        return str.at(0) == 'h';
+//    }));
 
     cout << endl;
 
-    printIt(filter(s, [](const string& str) {
-        return str.size() == 5;
-    }));
+    struct Lambda2
+    {
+        auto operator() (const string& str) const {
+            return str.size() == 5;
+        }
+    };
+
+    printIt(filter(s, Lambda2{}));
+//    printIt(filter(s, [](const string& str) {
+//        return str.size() == 5;
+//    }));
 
     cout << endl;
+
+//    struct Lambda3
+//    {
+//        auto operator() (double val) const {
+//            return val >= 0.0;
+//        }
+//    };
+
+    auto thresh = 0.0;
 
     auto v = list<double>{10.3, -5.8, 3.4};
-    printIt(filter(v, [](double val) {
-        return val >= 0.0;
-    }));
+//    printIt(filter(v, Lambda3{}));
+
+    struct Lambda4
+    {
+        double& thresh;
+
+        Lambda4(double& thresh) : thresh{thresh} { }
+
+        auto operator() (double val) const
+        {
+            return val >= thresh;
+        }
+    };
+
+
+//    auto l = [&thresh](double val) {
+//        return val >= thresh;
+//    };
+    auto l = Lambda4{thresh};
+
+    printIt(filter(v, l));
+
+    cout << endl;
+
+    thresh = 10.0;
+    printIt(filter(v, l));
 
     return 0;
 }
