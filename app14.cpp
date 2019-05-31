f12b10f5e388c98e52780bd3a38adc4698379e71
Furkan Kıraç <fkirac@gmail.com>
2019-05-31 08:58:53 +0300

    Lecture start.

---
+++ app14.cpp
@@ -0,0 +1,19 @@
+// returning a lambda (function object)
+// returning more than one parameter
+
+// std::pair, std::map, std::unordered_map, structured bindings
+// std::variant, std::any
+// structured bindings, returning more than one parameter
+
+// RVO: return value optimization
+
+// CRTP: curiously recurring template pattern, singleton pattern example
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
