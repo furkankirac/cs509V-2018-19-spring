40463b73b716acbe1cd6f53f30f0489ff3ccfaf1
Furkan Kıraç <fkirac@gmail.com>
2019-06-01 10:36:20 +0300

    Return Value Optimization.

--- app21.cpp
+++ app21.cpp
@@ -8,45 +8,80 @@
 
 using namespace std;
 
+struct VeryBig
+{
+    VeryBig() { cout << "constructed" << endl; }
+    ~VeryBig() { cout << "destructed" << endl; }
+
+    VeryBig(const VeryBig& other)
+    {
+        cout << "copy c-tor" << endl;
+    }
+
+    VeryBig(VeryBig&& other)
+    {
+        cout << "move c-tor" << endl;
+    }
+
+    void operator=(const VeryBig& other)
+    {
+        cout << "copy assign" << endl;
+    }
+
+    void operator=(VeryBig&& other)
+    {
+        cout << "move assign" << endl;
+    }
+};
+
+VeryBig foo()
+{
+    auto vb = VeryBig{};
+    return vb;
+}
+
 int main(int argc, char* argv[])
 {
-    using Variant = variant<int, double, string>;
+    auto bigobj = foo();
 
-    auto m = unordered_map<Variant, Variant>{
-        { "hello", "world" },
-        { "pi", 3.1415 },
-        { "apt.no", 7 },
-        { 10, "naber" }
-    };
 
-    for(const auto& [key, value] : m)
-    {
-        std::visit([](auto&& arg) {
-            cout << arg << endl;
+//    using Variant = variant<int, double, string>;
+
+//    auto m = unordered_map<Variant, Variant>{
+//        { "hello", "world" },
+//        { "pi", 3.1415 },
+//        { "apt.no", 7 },
+//        { 10, "naber" }
+//    };
+
+//    for(const auto& [key, value] : m)
+//    {
+//        std::visit([](auto&& arg) {
+//            cout << arg << endl;
+////            using T = decay_t<decltype(arg)>;
+////            if constexpr (is_same_v<T, int>)
+////                std::cout << "int with value " << arg << ',';
+////            else if constexpr (is_same_v<T, double>)
+////                std::cout << "double with value " << arg << ',';
+////            else if constexpr (is_same_v<T, string>)
+////                std::cout << "string with value \"" << arg << "\",";
+//        },
+//        key
+//        );
+
+//        std::visit([](auto&& arg) {
 //            using T = decay_t<decltype(arg)>;
 //            if constexpr (is_same_v<T, int>)
-//                std::cout << "int with value " << arg << ',';
+//                std::cout << "int with value " << arg << '\n';
 //            else if constexpr (is_same_v<T, double>)
-//                std::cout << "double with value " << arg << ',';
+//                std::cout << "double with value " << arg << '\n';
 //            else if constexpr (is_same_v<T, string>)
-//                std::cout << "string with value \"" << arg << "\",";
-        },
-        key
-        );
+//                std::cout << "string with value \"" << arg << "\"\n";
+//        },
+//        value
+//        );
 
-        std::visit([](auto&& arg) {
-            using T = decay_t<decltype(arg)>;
-            if constexpr (is_same_v<T, int>)
-                std::cout << "int with value " << arg << '\n';
-            else if constexpr (is_same_v<T, double>)
-                std::cout << "double with value " << arg << '\n';
-            else if constexpr (is_same_v<T, string>)
-                std::cout << "string with value \"" << arg << "\"\n";
-        },
-        value
-        );
-
-    }
+//    }
 
     return 0;
 }
